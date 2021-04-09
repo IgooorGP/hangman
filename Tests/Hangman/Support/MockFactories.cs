92f954095aa7b001aea83d89b7835f041d42e38e
IgooorGP <igor.feq@gmail.com>
2021-04-08 22:54:17 -0300

    simplify mock factories

--- Tests/Hangman/Support/MockFactories.cs
+++ Tests/Hangman/Support/MockFactories.cs
@@ -1,24 +1,10 @@
+using System.Text;
 using Bogus;
 using Hangman.Core.DTOs;
+using Hangman.Core.Models;
 
 namespace Tests.Hangman.Support
 {
-    public static class UserFakerFactory
-    {
-        public static Faker<CreateUserRequestDTO> CreateUserRequestDTOFaker()
-        {
-            return new CreateUserRequestFaker();
-        }
-    }
-
-    public static class GameRoomFactory
-    {
-        public static Faker<CreateGameRoomDTO> CreateGameRoomDTOFaker()
-        {
-            return new CreateGameRoomRequestFaker();
-        }
-    }
-
     public class CreateUserRequestFaker : Faker<CreateUserRequestDTO>
     {
         public CreateUserRequestFaker()
@@ -29,6 +15,19 @@ namespace Tests.Hangman.Support
             RuleFor(u => u.Password, f => f.Internet.Password(6));
         }
     }
+
+    public class CreateUserFaker : Faker<User>
+    {
+        public CreateUserFaker(string username = "TestUsername")
+        {
+            RuleFor(u => u.FirstName, f => f.Name.FirstName());
+            RuleFor(u => u.LastName, f => f.Name.LastName());
+            RuleFor(u => u.Username, (f, u) => username);
+            RuleFor(u => u.PasswordSalt, (f, u) => Encoding.UTF8.GetBytes(u.FirstName));  // simulates byte[]
+            RuleFor(u => u.PasswordDigest, (f, u) => Encoding.UTF8.GetBytes(u.LastName));  // simulates byte[]
+        }
+    }
+
     public class CreateGameRoomRequestFaker : Faker<CreateGameRoomDTO>
     {
         public CreateGameRoomRequestFaker()
